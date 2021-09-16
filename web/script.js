c4bdae01e9244b640b41e157f04304e12d18fed3
hellgrenj <hellgrenj@gmail.com>
2021-09-16 16:10:23 +0200

    first version of ugly web client

--- web/script.js
+++ web/script.js
@@ -1,21 +1,53 @@
-const elementExist = (elementId) => {
-    const el = document.getElementById(elementId)
-    return el !== undefined && el !== null;
-}
 document.addEventListener("DOMContentLoaded", async function () {
-    if (elementExist("observations")) {
-        const observationsDiv = document.getElementById("observations");
-        var observations = await GetObservations();
-        observations.forEach(ob => {
-            observationsDiv.innerHTML += `En IT-Konsult med beskrivningen "${ob.description}" observerades i ${ob.city} som har koordinaterna ${ob.lat} ${ob.long} <br/>`
-        })
-    } else {
-        console.log("element observations does not exist right now..");
-    }
+
+    await RenderObservations();
+    await AddEventListeners();
+
 });
+
+async function RenderObservations() {
+    const observationsDiv = document.getElementById("observations");
+    observationsDiv.innerHTML = ''
+    var observations = await GetObservations();
+    observations.forEach(ob => {
+        observationsDiv.innerHTML += `En IT-Konsult med beskrivningen "${ob.description}" observerades i ${ob.city} som har koordinaterna ${ob.lat} ${ob.long} <br/>`
+    })
+}
+async function AddEventListeners() {
+    const form = document.getElementById("createObservationForm");
+    form.addEventListener("submit", async function (evt) {
+        evt.preventDefault();
+        const observation = { city: document.getElementById("city").value, description: document.getElementById("description").value }
+        const result = await CreateObservation(observation)
+        console.log(`result ${result.success}`)
+        if(result.success) {
+            RenderObservations();
+        } else {
+            alert(result.message)
+        }
+    });
+}
 async function GetObservations() {
     var response = await fetch("http://localhost:1337/observation");
     var observations = await response.json();
     console.log(observations)
     return observations;
+}
+async function CreateObservation(observation) {
+    const response = await fetch('http://localhost:1337/observation', {
+        method: 'POST',
+        headers: {
+            'Accept': 'application/json',
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(observation)
+    });
+    if (response.status === 200) {
+        document.getElementById("city").value = ''
+        document.getElementById("description").value = ''
+        return { success: true, message: '' }
+    } else {
+        return { success: false, message: await response.json()}
+    }
+   
 }
